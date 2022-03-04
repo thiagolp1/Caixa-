f2c47c72dd0611d15267f30422ab360d0dc67296
thiago <thiagolopespereiralp1@gmail.com>
2022-03-03 22:41:18 -0300

    terminei a licao

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("+-----------+");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("|   thiago  |");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("+-----------+");
+Console.ResetColor();
